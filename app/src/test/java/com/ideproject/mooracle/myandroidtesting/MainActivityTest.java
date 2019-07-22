2c60859e693e352776dd771fd9187445c29f8700
mooracle <yanuar.prakosa@gmail.com>
2019-07-22 18:11:22 +0700

    Testing with Robolectric

--- app/src/test/java/com/ideproject/mooracle/myandroidtesting/MainActivityTest.java
+++ app/src/test/java/com/ideproject/mooracle/myandroidtesting/MainActivityTest.java
@@ -4,7 +4,9 @@ import android.view.inputmethod.EditorInfo;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,25 +16,25 @@ public class MainActivityTest {
     private MainActivity mainActivity;
     @Before
     public void setUp(){
-//        mainActivity = new MainActivity();
-//
-//        //set the onCreate method: (with saved instance null)
-//        mainActivity.onCreate(null);
+        mainActivity = Robolectric.buildActivity(MainActivity.class)
+                .create()
+                .resume()
+                .get();
     }
 
     // test the text editor views edit finish to see if ot will be shown in the text view
     @Test
     public void editTextUpdateTextView() throws Exception {
-//        //Arrange
-//        String givenString = "test 123";
-//        //NOTE: make the editText variable package private first to be accessed from here!!
-//        mainActivity.editText.setText(givenString);
-//
-//        //Act: set the onEditorAction method from Anonymous class TextView.OnEditorActionListener
-//        mainActivity.editText.onEditorAction(EditorInfo.IME_ACTION_DONE);
-//
-//        //Assert
-//        String actualString = mainActivity.textView.getText().toString();
-//        assertEquals(givenString,actualString);
+        //Arrange
+        String givenString = "test 123";
+        //NOTE: make the editText variable package private first to be accessed from here!!
+        mainActivity.editText.setText(givenString);
+
+        //Act: set the onEditorAction method from Anonymous class TextView.OnEditorActionListener
+        mainActivity.editText.onEditorAction(EditorInfo.IME_ACTION_DONE);
+
+        //Assert
+        String actualString = mainActivity.textView.getText().toString();
+        assertEquals(givenString,actualString);
     }
 }
